c3bf69a4df174d8423cd781c0bcd65a59f1cb4b1
Bryson Dunn <k.bryson.dunn@gmail.com>
2023-01-08 09:13:39 +0000

    Remove "All Zones" entity

--- custom_components/jellyfish_lighting/light.py
+++ custom_components/jellyfish_lighting/light.py
@@ -15,7 +15,6 @@ from .const import DOMAIN
 from . import JellyfishLightingDataUpdateCoordinator, JellyfishLightingApiClient
 from .entity import JellyfishLightingEntity
 
-_ALL_ZONES = "All Zones"
 _DEFAULT_RGB = (255, 193, 7)
 _DEFAULT_BRIGHTNESS = 255
 _LOGGER: logging.Logger = logging.getLogger(__package__)
@@ -28,8 +27,6 @@ async def async_setup_entry(hass, entry, async_add_devices):
         JellyfishLightingLight(coordinator, entry, zone)
         for zone in coordinator.api.zones
     ]
-    if len(lights) > 1:
-        lights.insert(0, JellyfishLightingLight(coordinator, entry, _ALL_ZONES))
     async_add_devices(lights)
 
 
@@ -52,7 +49,6 @@ class JellyfishLightingLight(JellyfishLightingEntity, LightEntity):
         self._attr_icon = "mdi:led-strip-variant"
         self._attr_assumed_state = False
         self.zone = zone
-        self.api_zone = None if self.zone == _ALL_ZONES else [self.zone]
         self.uid = re.sub("[^a-z0-9]", "_", zone.lower().strip("_"))
         self._attr_has_entity_name = True
         self._attr_name = zone
@@ -73,33 +69,18 @@ class JellyfishLightingLight(JellyfishLightingEntity, LightEntity):
         return await super().async_added_to_hass()
 
     async def async_refresh_data(self):
-        await self.api.async_get_zone_data(self.api_zone)
+        """Refresh data for this entity"""
+        await self.api.async_get_zone_data([self.zone])
         self._handle_coordinator_update()
 
     @callback
     def _handle_coordinator_update(self, *args) -> None:
-        if self.zone == _ALL_ZONES:
-            ons = set()
-            effects = set()
-            colors = set()
-            brightnesses = set()
-            for state in self.api.states.values():
-                ons.add(state[0])
-                effects.add(state[1])
-                colors.add(state[2])
-                brightnesses.add(state[3])
-            self._attr_is_on = len(ons) == 1 and ons.pop() == 1
-            self._attr_effect = effects.pop() if len(effects) == 1 else None
-            self._attr_rgb_color = colors.pop() if len(colors) == 1 else None
-            self._attr_brightness = (
-                brightnesses.pop() if len(brightnesses) == 1 else None
-            )
-        else:
-            state = self.api.states[self.zone]
-            self._attr_is_on = state[0] == 1
-            self._attr_effect = state[1]
-            self._attr_rgb_color = state[2]
-            self._attr_brightness = state[3]
+        """Handle updated data from the coordinator."""
+        state = self.api.states[self.zone]
+        self._attr_is_on = state[0] == 1
+        self._attr_effect = state[1]
+        self._attr_rgb_color = state[2]
+        self._attr_brightness = state[3]
 
         if self._attr_brightness is None:
             self._attr_brightness = 255
@@ -141,19 +122,19 @@ class JellyfishLightingLight(JellyfishLightingEntity, LightEntity):
             self._attr_brightness,
         )
         if self._attr_effect is not None:
-            await self.api.async_play_pattern(self._attr_effect, self.api_zone)
+            await self.api.async_play_pattern(self._attr_effect, [self.zone])
         elif self._attr_rgb_color is not None:
             await self.api.async_play_color(
                 self._attr_rgb_color,
                 int(self._attr_brightness / 255 * 100),
-                self.api_zone,
+                [self.zone],
             )
         else:
-            await self.api.async_turn_on(self.api_zone)
+            await self.api.async_turn_on([self.zone])
         await self.async_refresh_data()
 
     async def async_turn_off(self, **kwargs):  # pylint: disable=unused-argument
         """Turn off the light."""
         _LOGGER.debug("In async_turn_off for '%s'. kwargs is %s", self.zone, kwargs)
-        await self.api.async_turn_off(self.api_zone)
+        await self.api.async_turn_off([self.zone])
         await self.async_refresh_data()
