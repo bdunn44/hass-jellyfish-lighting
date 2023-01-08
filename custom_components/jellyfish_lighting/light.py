d69da61e1c01fc43c08362434bbc26d59369ddd5
Bryson Dunn <k.bryson.dunn@gmail.com>
2023-01-08 09:08:22 +0000

    Add RGB and concurrency support

--- custom_components/jellyfish_lighting/light.py
+++ custom_components/jellyfish_lighting/light.py
@@ -8,12 +8,16 @@ from homeassistant.components.light import (
     LightEntityFeature,
     ColorMode,
     ATTR_EFFECT,
+    ATTR_RGB_COLOR,
+    ATTR_BRIGHTNESS,
 )
 from .const import DOMAIN
-from . import JellyfishLightingDataUpdateCoordinator
+from . import JellyfishLightingDataUpdateCoordinator, JellyfishLightingApiClient
 from .entity import JellyfishLightingEntity
 
 _ALL_ZONES = "All Zones"
+_DEFAULT_RGB = (255, 193, 7)
+_DEFAULT_BRIGHTNESS = 255
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
@@ -40,11 +44,11 @@ class JellyfishLightingLight(JellyfishLightingEntity, LightEntity):
     ) -> None:
         """Initialize."""
         self._attr_supported_features = LightEntityFeature.EFFECT
-        self._attr_supported_color_modes = [
-            ColorMode.ONOFF,
-            # ColorMode.RGB # TODO: Add support for setting colors and brightness
-        ]
-        self._attr_color_mode = ColorMode.ONOFF
+        self._attr_supported_color_modes = {ColorMode.RGB}
+        self.api: JellyfishLightingApiClient = coordinator.api
+        self._attr_color_mode = ColorMode.RGB
+        self._attr_rgb_color = _DEFAULT_RGB
+        self._attr_brightness = _DEFAULT_BRIGHTNESS
         self._attr_icon = "mdi:led-strip-variant"
         self._attr_assumed_state = False
         self.zone = zone
@@ -62,27 +66,53 @@ class JellyfishLightingLight(JellyfishLightingEntity, LightEntity):
 
     @property
     def effect_list(self):
-        return self.coordinator.api.patterns
+        return self.api.patterns
+
+    async def async_added_to_hass(self) -> None:
+        self._handle_coordinator_update()
+        return await super().async_added_to_hass()
+
+    async def async_refresh_data(self):
+        await self.api.async_get_zone_data(self.api_zone)
+        self._handle_coordinator_update()
 
     @callback
     def _handle_coordinator_update(self, *args) -> None:
         if self.zone == _ALL_ZONES:
-            ons = []
+            ons = set()
             effects = set()
-            for state in self.coordinator.api.states.values():
-                ons.append(state[0])
+            colors = set()
+            brightnesses = set()
+            for state in self.api.states.values():
+                ons.add(state[0])
                 effects.add(state[1])
-            self._attr_is_on = all(ons)
+                colors.add(state[2])
+                brightnesses.add(state[3])
+            self._attr_is_on = len(ons) == 1 and ons.pop() == 1
             self._attr_effect = effects.pop() if len(effects) == 1 else None
+            self._attr_rgb_color = colors.pop() if len(colors) == 1 else None
+            self._attr_brightness = (
+                brightnesses.pop() if len(brightnesses) == 1 else None
+            )
         else:
-            state = self.coordinator.api.states[self.zone]
+            state = self.api.states[self.zone]
             self._attr_is_on = state[0] == 1
             self._attr_effect = state[1]
+            self._attr_rgb_color = state[2]
+            self._attr_brightness = state[3]
+
+        if self._attr_brightness is None:
+            self._attr_brightness = 255
+        else:
+            self._attr_brightness = int(self._attr_brightness / 100 * 255)
+
         _LOGGER.debug(
-            "Updated state for %s. On: %s, Effect: %s",
+            "Updated state for %s (state: %s, effect: %s, color: %s, brightness: %s)",
             self.zone,
-            self._attr_is_on,
+            "ON" if self._attr_is_on else "OFF",
             self._attr_effect,
+            self._attr_rgb_color,
+            self._attr_brightness,
         )
         self.async_write_ha_state()
 
@@ -91,15 +121,39 @@ class JellyfishLightingLight(JellyfishLightingEntity, LightEntity):
         _LOGGER.debug("In async_turn_on for '%s'. kwargs is %s", self.zone, kwargs)
         if ATTR_EFFECT in kwargs:
             self._attr_effect = kwargs.get(ATTR_EFFECT)
-            await self.coordinator.api.async_play_pattern(
-                self._attr_effect, self.api_zone
+        if ATTR_RGB_COLOR in kwargs:
+            self._attr_rgb_color = kwargs.get(ATTR_RGB_COLOR)
+            self._attr_effect = None
+        if ATTR_BRIGHTNESS in kwargs:
+            self._attr_brightness = kwargs.get(ATTR_BRIGHTNESS)
+            self._attr_effect = None
+
+        if self._attr_rgb_color is None:
+            self._attr_rgb_color = _DEFAULT_RGB
+        if self._attr_brightness is None:
+            self._attr_brightness = _DEFAULT_BRIGHTNESS
+
+        _LOGGER.debug(
+            "Turning on %s (effect: %s, color: %s, brightness: %s)",
+            self.zone,
+            self._attr_effect,
+            self._attr_rgb_color,
+            self._attr_brightness,
+        )
+        if self._attr_effect is not None:
+            await self.api.async_play_pattern(self._attr_effect, self.api_zone)
+        elif self._attr_rgb_color is not None:
+            await self.api.async_play_color(
+                self._attr_rgb_color,
+                int(self._attr_brightness / 255 * 100),
+                self.api_zone,
             )
         else:
-            await self.coordinator.api.async_turn_on(self.api_zone)
-        await self.coordinator.async_refresh()
+            await self.api.async_turn_on(self.api_zone)
+        await self.async_refresh_data()
 
     async def async_turn_off(self, **kwargs):  # pylint: disable=unused-argument
         """Turn off the light."""
         _LOGGER.debug("In async_turn_off for '%s'. kwargs is %s", self.zone, kwargs)
-        await self.coordinator.api.async_turn_off(self.api_zone)
-        await self.coordinator.async_refresh()
+        await self.api.async_turn_off(self.api_zone)
+        await self.async_refresh_data()
